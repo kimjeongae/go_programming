8451289788e6c280fe4195e61ca45d5744af6454
kjaa34@gmail.com <kjaa34@gmail.com>
2021-05-26 23:57:51 +0900

    1.7 finished.

--- learngo/main.go
+++ learngo/main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+	return true
 }
 
 func main() {
-	totalLength, name := lenAndUpper("kja")
-	fmt.Println(totalLength, name)
+	fmt.Println(canIDrink(15))
 }
