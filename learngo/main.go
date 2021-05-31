5eb2edba81014739e2c687ccfdbf34583ec93b7a
kjaa34@gmail.com <kjaa34@gmail.com>
2021-06-01 00:00:28 +0900

    1.11 finished.

--- learngo/main.go
+++ learngo/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	nico := map[string]string{"name": "aaa", "age": "20"}
-	for _, value := range nico {
-		fmt.Println(value)
-	}
+	favFood := []string{"aaa", "bbb"}
+	nico := person{name: "nico", age: 12, favFood: favFood}
+
+	fmt.Println(nico.favFood)
 }
